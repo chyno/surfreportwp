f6a3cb63b54ef55dcdf1678d0901089b1665f7b2
Maria Lopez <mane@Johns-MacBook-Air-2.local>
2016-09-15 22:26:19 -0400

    app works

--- src/welcome.js
+++ src/welcome.js
@@ -1,34 +1,56 @@
-//import {computedFrom} from 'aurelia-framework';
+import {inject} from 'aurelia-framework';
+import {HttpClient} from "aurelia-fetch-client";
+import {Service} from "./service";
 
+@inject(HttpClient, Service)
 export class Welcome {
-  heading = 'Welcome to the Aurelia Navigation App!';
-  firstName = 'John';
-  lastName = 'Doe';
-  previousValue = this.fullName;
 
-  //Getters can't be directly observed, so they must be dirty checked.
-  //However, if you tell Aurelia the dependencies, it no longer needs to dirty check the property.
-  //To optimize by declaring the properties that this getter is computed from, uncomment the line below
-  //as well as the corresponding import above.
-  //@computedFrom('firstName', 'lastName')
-  get fullName() {
-    return `${this.firstName} ${this.lastName}`;
-  }
-
-  submit() {
-    this.previousValue = this.fullName;
-    alert(`Welcome, ${this.fullName}!`);
-  }
-
-  canDeactivate() {
-    if (this.fullName !== this.previousValue) {
-      return confirm('Are you sure you want to leave?');
+    constructor(httpClient, service) {
+        this.service = service;
+        this.httpClient = httpClient;
+        this.heading = 'Current Forecast';
+        this.currently;
+        this.forecasts;
+        this.locations  = [];
+        this.selectedLocation = null;
     }
-  }
-}
 
-export class UpperValueConverter {
-  toView(value) {
-    return value && value.toUpperCase();
-  }
+    activate() {
+        
+        return this.service.getCurrentLocations().then(lcs =>
+        {
+           this.locations = lcs;
+           if (this.locations)
+           {
+               this.selectedLocation =  this.locations[0];
+               return this.renderForcast();
+           }
+        });
+        
+    }
+
+   renderForcast() {
+     
+     if (!this.selectedLocation)
+        {return;}
+         
+        return this.httpClient.fetch("api/zip/" + this.selectedLocation.zip)
+            .catch((r) => {
+              alert(r);
+        })
+        .then(response => 
+         {
+            if (response.ok) {
+                return response.json()
+            }
+            else {
+                return {};
+            }
+         })
+        .then(data => {
+           this.currently = data.currently;
+           this.forecasts = data.forecast;
+        });
+        
+    }
 }
